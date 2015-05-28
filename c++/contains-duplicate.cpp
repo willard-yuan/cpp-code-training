c9d514f50f0e80fc52bb6266d635006b93809f15
Yong Yuan <yongyuanstu@gmail.com>
2015-05-28 23:55:48 +0800

    Update contains-duplicate.cpp

--- c++/contains-duplicate.cpp
+++ c++/contains-duplicate.cpp
@@ -1,4 +1,7 @@
-/*Time Limit Exceeded*/
+/*直接遍历方法
+  时间复杂度太高O(n^2)
+  提交不能AC，时间溢出
+*/
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -12,5 +15,25 @@ public:
             }
         }
         return false;
+    }
+};
+
+
+/*采用哈希表
+  时间复杂度O(n)
+  基于哈希表的unordered_set，参考资料
+  1：http://www.zhihu.com/question/26078588
+  2：http://blog.csdn.net/u013195320/article/details/23046305
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if (int(nums.size())==0 || int(nums.size())==1)
+            return false;
+        std::unordered_set<int> set0(nums.begin(), nums.end());
+        if (nums.size() == set0.size())
+            return false;
+        else
+            return true;
     }
 };
